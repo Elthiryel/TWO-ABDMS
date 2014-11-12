2c18cfe86c2ca799d745dc951dc804f13e58e3ce
Marcin Łoś <los@student.agh.edu.pl>
2014-11-12 23:47:25 +0100

    Fix: MainWindow now has dimension set in ctor
    MainWindow initially is maximised. Its dimension was not set, which
    caused its drastic collapse to a really tiny narrow sliver (at least
    when run on Unity desktop) when the window is de-maximized. Now the size
    is set in the constructor, and MainWindow assumes it upon
    de-maximization.

--- src/main/java/pl/edu/agh/two/abdms/gui/components/MainWindow.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/components/MainWindow.java
@@ -3,6 +3,8 @@ package pl.edu.agh.two.abdms.gui.components;
 import pl.edu.agh.two.abdms.gui.components.menu.TopMenu;
 
 import javax.swing.JFrame;
+
+import java.awt.Dimension;
 import java.awt.HeadlessException;
 
 public class MainWindow extends JFrame {
@@ -11,6 +13,7 @@ public class MainWindow extends JFrame {
 
     public MainWindow() throws HeadlessException {
         setTitle("ABDMS");
+        setSize(new Dimension(600, 500));
         setExtendedState(getExtendedState() | MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         topMenu = new TopMenu();
