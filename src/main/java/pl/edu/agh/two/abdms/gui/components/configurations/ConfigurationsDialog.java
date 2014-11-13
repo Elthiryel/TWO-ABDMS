d7097905169f455b4d4137c1735b203d8fe84ef9
Marcin Łoś <los@student.agh.edu.pl>
2014-11-13 01:58:14 +0100

    Added filling ConfigurationsDialog table with data
    Model for table is created and used, but there is no way to parser
    arbitrary data file for now. Such functionality is out of scope of
    GUI/visualization task, and it would probably be useful in more
    situations. Once this is in place, this piece of visualization task can
    be completed.

--- src/main/java/pl/edu/agh/two/abdms/gui/components/configurations/ConfigurationsDialog.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/components/configurations/ConfigurationsDialog.java
@@ -1,7 +1,6 @@
 package pl.edu.agh.two.abdms.gui.components.configurations;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,6 +20,7 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import pl.edu.agh.two.abdms.data.loader.DataModel;
 import pl.edu.agh.two.abdms.gui.controller.Configuration;
 import pl.edu.agh.two.abdms.gui.controller.ConfigurationsController;
 
@@ -28,7 +28,7 @@ import pl.edu.agh.two.abdms.gui.controller.ConfigurationsController;
 public class ConfigurationsDialog extends JDialog {
     private final Container mainPanel;
     private final JButton newConfigurationButton;
-    private final JTable grid = new JTable();
+    private final JTable dataTable = new JTable();
 
     private final JList<Configuration> configurationList = new JList<>();
     private final ConfigurationsController configurationsController = 
@@ -37,35 +37,58 @@ public class ConfigurationsDialog extends JDialog {
     public ConfigurationsDialog() {
         mainPanel = this.getContentPane();
         mainPanel.setLayout(new BorderLayout());
-        
+
         setTitle("Data sources");
-        
+
         setSize(600, 500);
         setModal(true);
         newConfigurationButton = new JButton("Add new...");
-        
+
         configurationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         configurationList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 int index = e.getFirstIndex();
                 Configuration cfg = configurationList.getModel().getElementAt(index);
+                loadData(cfg);
             }
         });
-        
+
         configurationList.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         JScrollPane scroll = new JScrollPane(configurationList);
-        
-        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, grid);
+
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, dataTable);
         split.setDividerSize(5);
         split.setDividerLocation(180);
-        
+
         add(newConfigurationButton, BorderLayout.NORTH);
         add(split, BorderLayout.CENTER);
 
         loadConfigurationsList();
         bindListeners();
+    }
 
+    private void loadData(Configuration cfg) {
+        File file = cfg.getFile();
+        DataModel data = parseFile(file); 
+        DataTableModel model = new DataTableModel(data);
+        dataTable.setModel(model);
+    }
+
+    private DataModel parseFile(File file) {
+        // String extension = Files.getFileExtension(file.getName());
+
+        // TODO: Somehow load the data
+
+        // Ideally, there should be a class that manages available file format 
+        // parsers, and given a path it chooses appropriate parser, and consumes
+        // the file, producing DataModel object.
+        // Parsers should have uniform interface, and such parser manager should
+        // automatically discover implementations in the classpath.
+        // HERE is really not a good place to put this kind of logic.
+
+        // return result of aforementioned magic.
+        throw new UnsupportedOperationException();
     }
 
     private void loadConfigurationsList() {
