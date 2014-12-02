f455f4647a29867eb47cf8a3f7d8d8056f81fc59
Radek <matrallus@gmail.com>
2014-12-02 17:41:21 +0100

    - gui works - backend connection need

--- src/main/java/pl/edu/agh/two/abdms/gui/parameter_editor/ParameterEditorFrame.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/parameter_editor/ParameterEditorFrame.java
@@ -25,6 +25,11 @@ import javax.swing.JLabel;
 import com.sun.glass.ui.Size;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.TitledBorder;
 
 
 
@@ -55,17 +60,44 @@ public class ParameterEditorFrame extends JFrame {
 		textField.setColumns(10);
 		
 		JPanel patternPanel = new JPanel();
+		patternPanel.setLayout(new BoxLayout(patternPanel, BoxLayout.Y_AXIS));
+		patternPanel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		tabbedPane.addTab("Change by pattern", null, patternPanel, null);
-		patternPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		
+		JTextPane textPane = new JTextPane();
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		
+		JTextPane textPane_1 = new JTextPane();
+		//patternPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		table_1 = new JTable();
 		patternPanel.add(table_1);
+		patternPanel.add(lblNewLabel_1);
+		patternPanel.add(textPane);
+		patternPanel.add(lblNewLabel_2);
+		patternPanel.add(textPane_1);
 		
 		JPanel substringPanel = new JPanel();
+		substringPanel.setLayout(new BoxLayout(substringPanel, BoxLayout.Y_AXIS));
 		tabbedPane.addTab("Substring on column", null, substringPanel, null);
 		
 		table_2 = new JTable();
 		substringPanel.add(table_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		substringPanel.add(lblNewLabel_3);
+		
+		JTextPane textPane_2 = new JTextPane();
+		substringPanel.add(textPane_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		substringPanel.add(lblNewLabel_4);
+		
+		JTextPane textPane_3 = new JTextPane();
+		substringPanel.add(textPane_3);
 		
 		JList list = new JList();
 		getContentPane().add(list, BorderLayout.WEST);
