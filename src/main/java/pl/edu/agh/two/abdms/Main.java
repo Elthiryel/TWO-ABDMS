8c70a3bc6fb6474c283dada2b7a182df079d5256
Marek Nogacki <no.marek@gmail.com>
2014-12-03 18:42:13 +0100

    JADE

--- src/main/java/pl/edu/agh/two/abdms/Main.java
+++ src/main/java/pl/edu/agh/two/abdms/Main.java
@@ -1,12 +1,20 @@
 package pl.edu.agh.two.abdms;
 
+import jade.MicroBoot;
+import jade.core.MicroRuntime;
 import pl.edu.agh.two.abdms.gui.GuiLauncher;
 
-import java.io.File;
 
-public class Main {
+public class Main extends MicroBoot {
 
     public static void main(String[] args) {
+        MicroBoot.main(args);
+        try {
+            MicroRuntime.startAgent("MainAgent", "pl.edu.agh.two.abdms.agent.MainAgent", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         new GuiLauncher().startGui();
     }
+
 }
