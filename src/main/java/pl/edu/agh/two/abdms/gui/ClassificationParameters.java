ced042189c030b64a5f5ef0f34567283ceb62e47
konrad <jakubiec@student.agh.edu.pl>
2014-12-10 18:23:19 +0100

    Added classification parameters.

--- src/main/java/pl/edu/agh/two/abdms/gui/ClassificationParameters.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/ClassificationParameters.java
@@ -1,10 +1,34 @@
 package pl.edu.agh.two.abdms.gui;
 
+import java.util.Set;
+
 /**
  * Created by pawko on 2014-12-03.
  */
 public class ClassificationParameters implements ProcessParameters{
 
 
+	private Integer neighboursAmount;
+	private Set<String> choosenColumns;
+	private String classColumn;
+	
+	public Integer getNeighboursAmount() {
+		return neighboursAmount;
+	}
+	public void setNeighboursAmount(Integer neighboursAmount) {
+		this.neighboursAmount = neighboursAmount;
+	}
+	public Set<String> getChoosenColumns() {
+		return choosenColumns;
+	}
+	public void setChoosenColumns(Set<String> choosenColumns) {
+		this.choosenColumns = choosenColumns;
+	}
+	public String getClassColumn() {
+		return classColumn;
+	}
+	public void setClassColumn(String classColumn) {
+		this.classColumn = classColumn;
+	}
 
 }
--- src/main/java/pl/edu/agh/two/abdms/gui/components/classification/ClassificationWindow.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/components/classification/ClassificationWindow.java
@@ -3,13 +3,19 @@ package pl.edu.agh.two.abdms.gui.components.classification;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.Format;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import pl.edu.agh.two.abdms.data.loader.DataModel;
 import pl.edu.agh.two.abdms.gui.ClassificationParameters;
 import pl.edu.agh.two.abdms.gui.ProcessParametersView;
 
@@ -18,7 +24,8 @@ public class ClassificationWindow extends JFrame implements ProcessParametersVie
 	private static final String APPLY_CAPTION = "Apply";
 	private EventListener<ClassificationParameters> listener;
 	private JButton applyButton;
-	private JTextField parameterTextField;
+	private JTextField neighboursAmountTextField;
+	private JList<String> columnList;
 	
 	public ClassificationWindow() {
 		initComponents();
@@ -28,20 +35,21 @@ public class ClassificationWindow extends JFrame implements ProcessParametersVie
 
 	private void initComponents() {
 		applyButton = new JButton(APPLY_CAPTION);
-		parameterTextField = new JTextField();
+		neighboursAmountTextField = new JTextField();
+		columnList = new JList<>(DataModel.)
 	}
 	
 	private void setProperties() {
 		setVisible(false);
 		setLayout(new BorderLayout());
-		parameterTextField.setColumns(15);
+		neighboursAmountTextField.setColumns(15);
 		setSize(300, 300);
 		setLocationRelativeTo(null);
 	}
 	
 	private void addComponents() {
 		add(applyButton, BorderLayout.PAGE_END);
-		add(wrapWithLabel(parameterTextField, "ClassificationParameters"), BorderLayout.CENTER);
+		add(wrapWithLabel(neighboursAmountTextField, "Neighbours amount"), BorderLayout.LINE_START);
 	}
 	
 	private Component wrapWithLabel(Component componentToWrap, String caption) {
