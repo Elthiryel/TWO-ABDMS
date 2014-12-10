c4fc1807bb40498edc6724e8f8e5c9820ccfc54c
konrad <jakubiec@student.agh.edu.pl>
2014-12-10 19:21:52 +0100

    Added listener to classification window

--- src/main/java/pl/edu/agh/two/abdms/gui/ClassificationParameters.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/ClassificationParameters.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.two.abdms.gui;
 
-import java.util.Set;
+import java.util.Collection;
 
 /**
  * Created by pawko on 2014-12-03.
@@ -9,19 +9,26 @@ public class ClassificationParameters implements ProcessParameters{
 
 
 	private Integer neighboursAmount;
-	private Set<String> choosenColumns;
+	private Double learningDataScope;
+	private Collection<String> choosenColumns;
 	private String classColumn;
 	
+	public Double getLearningDataScope() {
+		return learningDataScope;
+	}
+	public void setLearningDataScope(Double learningDataScope) {
+		this.learningDataScope = learningDataScope;
+	}
 	public Integer getNeighboursAmount() {
 		return neighboursAmount;
 	}
 	public void setNeighboursAmount(Integer neighboursAmount) {
 		this.neighboursAmount = neighboursAmount;
 	}
-	public Set<String> getChoosenColumns() {
+	public Collection<String> getChoosenColumns() {
 		return choosenColumns;
 	}
-	public void setChoosenColumns(Set<String> choosenColumns) {
+	public void setChoosenColumns(Collection<String> choosenColumns) {
 		this.choosenColumns = choosenColumns;
 	}
 	public String getClassColumn() {
--- src/main/java/pl/edu/agh/two/abdms/gui/components/classification/ClassificationWindow.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/components/classification/ClassificationWindow.java
@@ -1,21 +1,13 @@
 package pl.edu.agh.two.abdms.gui.components.classification;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
-import java.text.DecimalFormat;
-import java.text.Format;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFormattedTextField;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-import pl.edu.agh.two.abdms.data.loader.DataModel;
 import pl.edu.agh.two.abdms.gui.ClassificationParameters;
 import pl.edu.agh.two.abdms.gui.ProcessParametersView;
 
@@ -26,32 +18,52 @@ public class ClassificationWindow extends JFrame implements ProcessParametersVie
 	private JButton applyButton;
 	private JTextField neighboursAmountTextField;
 	private JList<String> columnList;
-	
+	private DefaultListModel<String> listModel;
+	private JTextField learningDataScopeTextField;
+	private JList<String> classColumnList;
+	private ClassificationParameters params;
+
 	public ClassificationWindow() {
 		initComponents();
 		setProperties();
 		addComponents();
+		setListeners();
 	}
 
 	private void initComponents() {
 		applyButton = new JButton(APPLY_CAPTION);
 		neighboursAmountTextField = new JTextField();
-		columnList = new JList<>(DataModel.)
+		learningDataScopeTextField = new JTextField();
+		listModel = new DefaultListModel<>();
+		columnList = new JList<>(listModel);
+		classColumnList = new JList<>(listModel);
 	}
-	
+
 	private void setProperties() {
 		setVisible(false);
-		setLayout(new BorderLayout());
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		neighboursAmountTextField.setColumns(15);
+		learningDataScopeTextField.setColumns(15);
 		setSize(300, 300);
 		setLocationRelativeTo(null);
+		columnList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		columnList.setLayoutOrientation(JList.VERTICAL);
+		classColumnList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		classColumnList.setLayoutOrientation(JList.VERTICAL);
 	}
-	
+
 	private void addComponents() {
-		add(applyButton, BorderLayout.PAGE_END);
-		add(wrapWithLabel(neighboursAmountTextField, "Neighbours amount"), BorderLayout.LINE_START);
+		add(wrapWithLabel(neighboursAmountTextField, "Neighbours amount:"));
+		add(new JSeparator(SwingConstants.HORIZONTAL));
+		add(wrapWithLabel(columnList, "Columns:"));
+		add(new JSeparator(SwingConstants.HORIZONTAL));
+		add(wrapWithLabel(learningDataScopeTextField, "Learning data (0.0-1.0):"));
+		add(new JSeparator(SwingConstants.HORIZONTAL));
+		add(wrapWithLabel(classColumnList, "Class column (only one):"));
+		add(new JSeparator(SwingConstants.HORIZONTAL));
+		add(applyButton);
 	}
-	
+
 	private Component wrapWithLabel(Component componentToWrap, String caption) {
 		JPanel panel = new JPanel(new FlowLayout());
 		JLabel label = new JLabel(caption);
@@ -60,6 +72,34 @@ public class ClassificationWindow extends JFrame implements ProcessParametersVie
 		return panel;
 	}
 
+	private void setListeners() {
+		applyButton.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				try{
+					Integer neighbourAmount = Integer.valueOf(neighboursAmountTextField.getText());
+					Double learningDataScope = Double.valueOf(learningDataScopeTextField.getText());
+					List<String> selectedColumns = columnList.getSelectedValuesList();
+					String classColumn = classColumnList.getSelectedValue();
+					params.setChoosenColumns(selectedColumns);
+					params.setClassColumn(classColumn);
+					params.setLearningDataScope(learningDataScope);
+					params.setNeighboursAmount(neighbourAmount);
+				}catch(NumberFormatException e){
+					recoverError(e.getMessage());
+				}
+			}
+		});
+				
+		
+	}
+	
+	private void recoverError(String msg) {
+		JOptionPane.showMessageDialog(this, msg,
+				  "Error", JOptionPane.ERROR_MESSAGE);
+	}
+	
 	@Override
 	public void setEventListener(EventListener<ClassificationParameters> listener) {
 		this.listener = listener;
@@ -67,6 +107,21 @@ public class ClassificationWindow extends JFrame implements ProcessParametersVie
 
 	@Override
 	public void displayData(ClassificationParameters params) {
+		this.params = params;
+		/*
+		 * ConfigurationData currentDataConfiguration =
+		 * ApplicationData.getCurrentDataConfiguration();
+		 * if(currentDataConfiguration != null &&
+		 * currentDataConfiguration.getDataModel() != null) {
+		 * Arrays.stream(currentDataConfiguration
+		 * .getDataModel().getColumnValues()) .forEach( (element) ->
+		 * listModel.addElement(element)); setVisible(true); } else
+		 * JOptionPane.showMessageDialog(this, "Please load data first",
+		 * "Error", JOptionPane.ERROR_MESSAGE);
+		 */
+		listModel.addElement("1st col");
+		listModel.addElement("2nd col");
+		listModel.addElement("3rd col");
 		setVisible(true);
 	}
 
