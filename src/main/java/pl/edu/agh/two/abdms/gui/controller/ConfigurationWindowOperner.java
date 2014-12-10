96d4349250a3f73b251ad8622e02cd12e7319ad5
luks91 <szczyglowski.lukasz@gmail.com>
2014-12-10 18:21:56 +0100

    - 2 of 3 interactions on dataset implemented

--- src/main/java/pl/edu/agh/two/abdms/gui/controller/ConfigurationWindowOperner.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/controller/ConfigurationWindowOperner.java
@@ -1,7 +1,11 @@
 package pl.edu.agh.two.abdms.gui.controller;
 
+import javax.swing.JFrame;
+
 import pl.edu.agh.two.abdms.gui.ClassificationParameters;
+import pl.edu.agh.two.abdms.gui.DataPrepareParameters;
 import pl.edu.agh.two.abdms.gui.ProcessParameters;
+import pl.edu.agh.two.abdms.gui.parameter_editor.ParameterEditorFrame;
 
 public class ConfigurationWindowOperner {
 
@@ -14,12 +18,19 @@ public class ConfigurationWindowOperner {
 
         if (processParameters instanceof ClassificationParameters) {
             openClassificationWindow();
+        } else if (processParameters instanceof DataPrepareParameters) {
+        	openDataPrepareParametersWindow();
         }
 
     }
 
     private void openClassificationWindow() {
-
+    	
     }
 
+    private void openDataPrepareParametersWindow() {
+    	JFrame dataPrepareFrame = new ParameterEditorFrame(null);
+    	dataPrepareFrame.pack();
+    	dataPrepareFrame.setVisible(true);
+    }
 }
--- src/main/java/pl/edu/agh/two/abdms/gui/parameter_editor/ParameterEditorFrame.java
+++ src/main/java/pl/edu/agh/two/abdms/gui/parameter_editor/ParameterEditorFrame.java
@@ -1,10 +1,10 @@
 package pl.edu.agh.two.abdms.gui.parameter_editor;
 
-import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-
-import javafx.geometry.Dimension2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -19,27 +19,39 @@ import javax.swing.JTextPane;
 import javax.swing.border.TitledBorder;
 
 import pl.edu.agh.two.abdms.data.loader.DataModel;
-import javax.swing.SwingConstants;
-import java.awt.Component;
+import pl.edu.agh.two.abdms.util.ActionCommand;
+import pl.edu.agh.two.abdms.util.ProcessState;
 
 public class ParameterEditorFrame extends JFrame {
+
+	private static final long serialVersionUID = -4240427884312513747L;
+	
 	private JTable dataModelTable;
 	private JTextField newValuePane;
+	private JTextPane findPane;
+	private JTextPane replacePane;
+	private JTextPane indexFromPane;
+	private JTextPane indexToPane;
+	private JTabbedPane tabbedPane;
+	private OnActionCommandReceivedListener mCommandReceivedListener = new EmptyActionCommandListener();
 	
 	public ParameterEditorFrame(DataModel dataModel) {
 		
 		getContentPane().setLayout(
 				new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-		dataModelTable = new JTable(DataModelExtractor.parseDataModel(dataModel),dataModel.getColumnValues());
+		dataModelTable = new ImmutableJTable(
+				DataModelExtractor.parseDataModel(dataModel),
+				dataModel.getColumnValues());
+		dataModelTable.setCellSelectionEnabled(true);
 		dataModelTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		JScrollPane scrollPane = new JScrollPane(dataModelTable);
 		scrollPane.setSize(100, 20);
 		getContentPane().add(scrollPane);
 
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		getContentPane().add(tabbedPane);
-
+		
 		JPanel valuePanel = new JPanel();
 		tabbedPane.addTab("Change value", null, valuePanel, null);
 		valuePanel.setLayout(new GridBagLayout());
@@ -54,6 +66,7 @@ public class ParameterEditorFrame extends JFrame {
 		patternPanel.setLayout(new GridLayout(4,1));
 		patternPanel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
+
 		JLabel findWhatOfPatternLabel = new JLabel("Find what");
 		JTextPane findWhatPatternPane = new JTextPane();
 		JLabel ReplaceToOfPatternLabel = new JLabel("Replace to");
@@ -81,9 +94,137 @@ public class ParameterEditorFrame extends JFrame {
 		substringPanel.add(scrollPaneOfFindWhatOfSubstringPane);
 		substringPanel.add(replaceToSubstringLabel);
 		substringPanel.add(scrollPaneOfReplaceToOfSubstringPane);
-	
 		
 		JButton changeButton = new JButton("Change");
+		changeButton.addActionListener(new ChangeButtonClickedActionListener());
 		getContentPane().add(changeButton);
+	}
+	
+	public final void setOnActionCommandReceivedListener(OnActionCommandReceivedListener listener) {
+		if (listener == null) {
+			mCommandReceivedListener = new EmptyActionCommandListener();
+			return;
+		}
+		
+		mCommandReceivedListener = listener;
+	}
+	
+	private class ChangeButtonClickedActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ActionCommand requestedCommand = constructRequestedCommand();
+			mCommandReceivedListener.onActionCommandReceived(requestedCommand);
+			setVisible(false);
+			dispose();
+		}
+		
+		private ActionCommand constructRequestedCommand() {
+			
+			String primaryModificationValue = null;
+			String secondaryModificationValue = null;
+			SelectedOperationType operationType = null;
+			
+			switch(tabbedPane.getSelectedIndex()) {
+			case 0:
+				operationType = SelectedOperationType.CHANGE_VALUE;
+				primaryModificationValue = newValuePane.getText();
+				break;
+			case 1:
+				operationType = SelectedOperationType.CHANGE_PATTERN;
+				primaryModificationValue = findPane.getText();
+				secondaryModificationValue = replacePane.getText();
+				break;
+			case 2:
+				operationType = SelectedOperationType.SUBSTRING_ON_COLUMN;
+				primaryModificationValue = indexFromPane.getText();
+				secondaryModificationValue = indexToPane.getText();
+				break;
+			}
+			
+			final String selectedColumnName = dataModelTable.getSelectedColumn() > 0 ?
+					(String) dataModelTable.getColumnName(dataModelTable.getSelectedColumn())
+					: null;
+			final int selectedCellRow = dataModelTable.getSelectedRow();
+
+			return new ReturnActionCommand(selectedCellRow, selectedColumnName,
+					operationType, primaryModificationValue, secondaryModificationValue);
+		}
+	}
+	
+	private static final class ReturnActionCommand implements ActionCommand {
+		
+		private final int mRow;
+		private final String mColumnName;
+		private final SelectedOperationType mOperationType;
+		private final String mPrimaryValue;
+		private final String mSecondaryValue;
+		
+		public ReturnActionCommand(int row, String columnName, SelectedOperationType operationType, 
+				String primaryValue, String secondaryValue) {
+			
+			mRow = row;
+			mColumnName = columnName;
+			mOperationType = operationType;
+			mPrimaryValue = primaryValue;
+			mSecondaryValue = secondaryValue;
+		}
+
+		@Override
+		public void performAction(ProcessState object) {
+			mOperationType.performOperation(object.getDataModel(), mRow,
+					mColumnName, mPrimaryValue, mSecondaryValue);
+		}
+	}
+	
+	private enum SelectedOperationType {
+		CHANGE_VALUE {
+			@Override
+			void performOperation(DataModel dataModel, int row, String columnName,
+					String primaryValue, String secondaryValue) {
+				
+				if (row < 0 || dataModel == null)
+					return;
+				
+				Map<String, String> valuesMap = dataModel.getRow(row);
+				
+				if (valuesMap == null)
+					return;
+				
+				valuesMap.put(columnName, primaryValue);
+				
+			}
+		}, CHANGE_PATTERN {
+			@Override
+			void performOperation(DataModel dataModel, int row, String columnName,
+					String primaryValue, String secondaryValue) {
+				
+				for (Map<String, String> rowMap : dataModel.getValues()) {
+					for (String k : rowMap.keySet()) {
+						if (!rowMap.get(k).equals(primaryValue))
+							continue;
+						
+						rowMap.put(k, secondaryValue);
+					}
+				}
+			}
+		}, SUBSTRING_ON_COLUMN {
+			@Override
+			void performOperation(DataModel dataModel, int row, String columnName,
+					String primaryValue, String secondaryValue) {
+				
+				//TODO: implement
+				throw new UnsupportedOperationException("Not implemented");
+			}
+		};
+		
+		abstract void performOperation(DataModel dataModel, int row, 
+				String columnName, String primaryValue, String secondaryValue);
+	}
+	
+	private static final class EmptyActionCommandListener implements OnActionCommandReceivedListener {
+		@Override
+		public void onActionCommandReceived(ActionCommand actionCommand) {
+			//swallow
+		}
 	}
 }
